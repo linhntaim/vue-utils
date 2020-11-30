d0fedf5758a097bd18f6ee1ab00d637b52fa7907
Nguyen Tuan Linh <tuanlinh.nguyen@dsquare.com.vn>
2020-11-30 10:12:33 +0700

    v1.1.0

---
+++ src/cookie/cookie-stores/bearer-token-cookie-store.js
@@ -0,0 +1,18 @@
+import {CookieStore} from './cookie-store'
+
+export class BearerTokenCookieStore extends CookieStore {
+    transform(value) {
+        return value ? {
+            accessToken: value.accessToken,
+            tokenType: value.tokenType,
+            refreshToken: value.refreshToken,
+            tokenEndTime: value.tokenEndTime,
+        } : null
+    }
+
+    store(value, expires = 365) {
+        this.value = this.transform(value)
+        this.cookieHandler.set(this.name, this.value, new Date(value.tokenEndTime))
+        return this.value
+    }
+}
--- src/cookie/cookie-stores/passport-cookie-store.js
+++ src/cookie/cookie-stores/passport-cookie-store.js
@@ -1,12 +1,4 @@
-import {CookieStore} from './cookie-store'
+import {BearerTokenCookieStore} from './bearer-token-cookie-store'
 
-export class PassportCookieStore extends CookieStore {
-    transform(value) {
-        return value ? {
-            accessToken: value.accessToken,
-            tokenType: value.tokenType,
-            refreshToken: value.refreshToken,
-            tokenEndTime: value.tokenEndTime,
-        } : null
-    }
+export class PassportCookieStore extends BearerTokenCookieStore {
 }
