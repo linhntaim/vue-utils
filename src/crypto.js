38886f57d7565c3a7e48bdb3f778033a8a1c6be6
Nguyen Tuan Linh <tuanlinh.nguyen@dsquare.com.vn>
2021-04-01 11:25:30 +0700

    v1.4.0

--- src/crypto.js
+++ src/crypto.js
@@ -13,7 +13,7 @@ export class Crypto {
         return CryptoJS.AES.encrypt(text, this.getSecret(secret)).toString()
     }
 
-    decrypt(text, secret) {
+    decrypt(text, secret = null) {
         try {
             return CryptoJS.AES.decrypt(text, this.getSecret(secret)).toString(CryptoJS.enc.Utf8)
         } catch (e) {
@@ -23,20 +23,58 @@ export class Crypto {
         }
     }
 
-    encryptJson(data, secret) {
-        return this.encrypt(JSON.stringify(data), this.getSecret(secret))
+    encryptJson(data, secret = null) {
+        return this.encrypt(this.toJson(data), secret)
     }
 
-    decryptJson(text, secret) {
-        return JSON.parse(this.decrypt(text, this.getSecret(secret)))
+    decryptJson(text, secret = null) {
+        return this.fromJson(this.decrypt(text, secret))
     }
 
-    encryptBase64(text) {
+    encodeBase64(text) {
         return CryptoJS.enc.Base64.stringify(CryptoJS.enc.Utf8.parse(text))
     }
 
-    decryptBase64(encodedText) {
+    decodeBase64(encodedText) {
         return CryptoJS.enc.Utf8.stringify(CryptoJS.enc.Base64.parse(encodedText))
+    }
+
+    encodeBase64Json(data) {
+        return this.encodeBase64(this.toJson(data))
+    }
+
+    decodeBase64Json(encodedText) {
+        return this.fromJson(this.decodeBase64(encodedText))
+    }
+
+    encryptBase64(text, secret = null) {
+        return this.encodeBase64(this.encrypt(text, secret))
+    }
+
+    decryptBase64(encodedText, secret = null) {
+        return this.decrypt(this.decodeBase64(encodedText), secret)
+    }
+
+    encryptBase64Json(data, secret = null) {
+        return this.encryptBase64(this.toJson(data), secret)
+    }
+
+    decryptBase64Json(encodedText, secret = null) {
+        return this.fromJson(this.decryptBase64(encodedText, secret))
+    }
+
+    toJson(data) {
+        return JSON.stringify(data)
+    }
+
+    fromJson(text) {
+        try {
+            return JSON.parse(text)
+        } catch (e) {
+            // eslint-disable-next-line no-console
+            console.log(e)
+            return null
+        }
     }
 }
 
